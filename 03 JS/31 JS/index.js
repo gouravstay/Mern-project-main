aa612496881873a58e19b43ecf08b294decdbae7
Gaurav Paul <124757807+gouravstay@users.noreply.github.com>
2024-11-02 13:10:07 +0530

    added varribe file changes

--- 03 JS/31 JS/index.js
+++ 03 JS/31 JS/index.js
@@ -5,6 +5,7 @@ let a = 10;
 a = 30 // manipulation
 console.log(a)
 
+//let
  let x = 50;
  x = "gaurav"
  x = true;
@@ -12,5 +13,12 @@ console.log(a)
  //constant 
 const  b = 20;
 
- b = "Gaurav"
-  console.log(b)
+//  b = "Gaurav"
+//   console.log(b)
+
+//Data type: primitive
+let marks = 5625623651651312351212151212151325484842513 ; 
+console.log(marks);
+
+let marks2 = null;
+console.log(typeof(marks2))
