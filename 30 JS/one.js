b596a79075e726a8db4565cc921833633567f230
Gaurav Paul <124757807+gouravstay@users.noreply.github.com>
2024-11-02 12:50:19 +0530

    added file changes

--- 30 JS/one.js
+++
@@ -1,4 +0,0 @@
-console.log("hello world")
-console.log("hello, How are you!")
-console.log("hello, i want to meet you")
-console.log("hello, what about you...")
--- 40 JS/script.js
+++
@@ -1,9 +0,0 @@
-console.log('hello world')
-
-document.body.firstChild
-document.body.firstElementChild
-document.body.firstElementChild.children[3]
-document.body.firstElementChild.children[3].parentElement
-document.body.firstElementChild.children[3].previousElementSibling
-document.body.firstElementChild.children[3].previousSibling
-document.body.children[1].nextElementSibling
